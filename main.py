4d0a17f39a28e95444d39c99cecca73f8a5cf588
ptgmm <ptgmm@proton.me>
2024-05-25 22:07:54 +0200

    Restoring the previous program

--- main.py
+++ main.py
@@ -1,51 +1,63 @@
 #!/usr/bin/env python
 
+import requests
+import json
 import sys
 import os
-import nmap
-from scripts import ipgeo
-
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
-
 ascii_art = """
- _                                  
-(_)                                 
- _ _ __  _ __ ___   ___ _ __  _   _ 
-| | '_ \\| '_ ` _ \\ / _ \\ '_ \\| | | |
-| | |_) | | | | | |  __/ | | | |_| |
-|_| .__/|_| |_| |_|\\___|_| |_|\\__,_|
-  | |                               
-  |_|                               
-                                                                       
+  _____ _____     _____            _                 _   _
+ |_   _|  __ \\   / ____|          | |               | | (_)
+   | | | |__) | | |  __  ___  ___ | | ___   ___ __ _| |_ _  ___  _ __
+   | | |  ___/  | | |_ |/ _ \\/ _ \\| |/ _ \\ / __/ _` | __| |/ _ \\| '_ \\
+  _| |_| |      | |__| |  __/ (_) | | (_) | (_| (_| | |_| | (_) | | | |
+ |_____|_|       \\_____|\\___|\\___/|_|\\___/ \\___\\__,_|\\__|_|\\___/|_| |_|
+
 """
 
+print(ascii_art)
+
+def program ():
+    print("__________________________")
+    ip = input('Enter IP Addres: ')
+    url = requests.get(f'https://ipinfo.io/{ip}')
+
+    output = json.loads(url.text)
+
+    hostname = (output['hostname'])
+    country = (output['country'])
+    region = (output['region'])
+    city = (output['city'])
+    loc = (output['loc'])
+    timezone = (output['timezone'])
+    postal = (output['postal'])
+
+    print(f'Hostname: {hostname}')
+    print(f'Country: {country}')
+    print(f'Region: {region}')
+    print(f'City: {city}')
+    print(f'Coordinates: {loc}')
+    print(f'Timezone {timezone}')
+    print(f'Postal: {postal}')
+
+    split = loc.split(',')
+    print(f'Google Maps: https://www.google.com/maps/?q={split[0]},{split[1]}')
+
+program()
 
 
-def menu():
+def loop():
 
-    print(ascii_art)
-    print("ipgeo - 1") 
-    print("nmap - 2")
-
-    menu = input("")
-
-    if menu == "1":
-        os.system('cls' if os.name == 'nt' else 'clear')
-        ipgeo.program()
-    elif menu == "2":
-        os.system('cls' if os.name == 'nt' else 'clear')
-        nmap.program()
+    print("__________________________")
+    x = int(input("0 - Exit/ 1 - Continue: "))
+    if x == 0:
+        sys.exit()
+    elif x == 1:
+        program()
     else:
         print("error")
-menu()
 
-
-sth = 1
-while sth > 0:
-    ipgeo.loop()
-
-
-
-
+while True:
+    loop()
